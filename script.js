18c2c6ac84d9ca4d269fb86ca5299955bbea0749
DiegoRoa-67000500 <124110619+DiegoRoa-67000500@users.noreply.github.com>
2023-04-20 21:37:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
+// Named Function para sumar
 function sumar() {
-	var num1 = parseFloat(document.getElementById("num1").value);
-	var num2 = parseFloat(document.getElementById("num2").value);
-	var resultado = num1 + num2;
-	document.getElementById("resultado").value = resultado;
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").value = resultado;
 }
 
-function restar() {
-	var num1 = parseFloat(document.getElementById("num1").value);
-	var num2 = parseFloat(document.getElementById("num2").value);
-	var resultado = num1 - num2;
-	document.getElementById("resultado").value = resultado;
-}
+// Anonymous Function para restar
+var restar = function() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").value = resultado;
+};
 
-function raiz() {
-	var num1 = parseFloat(document.getElementById("num1").value);
-	var resultado = Math.sqrt(num1);
-	document.getElementById("resultado").value = resultado;
-}
+// Arrow Function para multiplicar
+const multiplicar = () => {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("resultado").value = resultado;
+};
 
-function exponente() {
-	var num1 = parseFloat(document.getElementById("num1").value);
-	var num2 = parseFloat(document.getElementById("num2").value);
-	var resultado = Math.pow(num1, num2);
-	document.getElementById("resultado").value = resultado;
-}
-
-
-function dividir() {
+// IIFE Function para dividir
+(function dividir() {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
     if (num2 == 0) {
@@ -35,4 +32,25 @@ function dividir() {
     }
     var resultado = num1 / num2;
     document.getElementById("resultado").value = resultado;
+})();
+
+// Higher Order Function para la raiz
+function raiz() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var resultado = calcularOperacion(num1, Math.sqrt);
+    document.getElementById("resultado").value = resultado;
 }
+
+function calcularOperacion(num, operacion) {
+    return operacion(num);
+}
+
+// Constructor Function para el exponente
+function exponente() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = Math.pow(num1, num2);
+    document.getElementById("resultado").value = resultado;
+}
+
+var exponente = new Exponente();
